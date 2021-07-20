81e9bafa06ffadaa31e4a2e6609c8a2d237da13c
Carissa Farry <cazzahra31@gmail.com>
2021-07-20 23:08:49 +0700

    #14 Outputting Lists

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      // * Each array item is a object
+      {name: 'Ryu', age: 30, belt: 'black', id: 1},
+      {name: 'Yoshi', age: 30, belt: 'green', id: 2},
+      {name: 'Crystal', age: 30, belt: 'pink', id: 3},
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My first react app!</h1>
         <p>Welcome :)</p>
 
-        <Ninjas name="Ryu" age="25" belt="black"/>
+        {/* <Ninjas name="Ryu" age="25" belt="black"/> */}
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     )
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -7,13 +7,27 @@ class Ninjas extends Component {
         // * DESTRUCTURING PROPERTIES / PROPS
         // ? simpan props name ke variabel bernama "name", dst (nama variabel harus sama dengan nama props)
         // ? Destructuring memungkinkan agar tidak perlu selalu memanggil props dengan "this.state"
-        const { name, age, belt } = this.props;
-        
+        // const { name, age, belt } = this.props;
+
+        const { ninjas } = this.props;
+        // const ninjas = this.props.ninjas;
+
+        // * Membuat konstanta array yang menyimpan per baris data array ninjas (array 2 dimensi menjadi 1 dimensi)
+        const ninjaList = ninjas.map(ninja => {
+            return (
+                <div className="ninja" key={ ninja.id }>
+                    <div>Name: { ninja.name }</div>
+                    <div>Age: { ninja.age }</div>
+                    <div>Belt: { ninja.belt }</div>
+                    <hr />
+                </div>
+            )
+        })
+
+        // * Menampilkan nilai konstanta array ninjaList sebanyak data mapping yang dihasilkan
         return(
-            <div className="ninja">
-            <div>Name: {name}</div>
-            <div>Age: {age}</div>
-            <div>Belt: {belt}</div>
+            <div className="ninja-list">
+                { ninjaList }
             </div>
         )
     }
