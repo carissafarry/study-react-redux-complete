6e8fd852bae736d7006e78b07dfa0bf64b70973f
Carissa Farry <cazzahra31@gmail.com>
2021-07-20 18:27:35 +0700

    #13 Props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
         <h1>My first react app!</h1>
         <p>Welcome :)</p>
 
-        <Ninjas />
+        <Ninjas name="Ryu" age="25" belt="black"/>
       </div>
     )
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component {
     render() {
+        // console.log(this.props);
+
+        // * DESTRUCTURING PROPERTIES / PROPS
+        // ? simpan props name ke variabel bernama "name", dst (nama variabel harus sama dengan nama props)
+        // ? Destructuring memungkinkan agar tidak perlu selalu memanggil props dengan "this.state"
+        const { name, age, belt } = this.props;
+        
         return(
             <div className="ninja">
-            <div>Name: Ryu</div>
-            <div>Age: 30</div>
-            <div>Belt: Black</div>
+            <div>Name: {name}</div>
+            <div>Age: {age}</div>
+            <div>Belt: {belt}</div>
             </div>
         )
     }
