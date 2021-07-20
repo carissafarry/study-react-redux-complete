1cc98348ef36badfedb6f92714e4f6ca9fcb45be
Carissa Farry <cazzahra31@gmail.com>
2021-07-20 23:28:31 +0700

    #15 Stateless Components

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,36 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render() {
-        // console.log(this.props);
+// ! Only use functional components when we don't need the state and we are only primarily concerned with the UI or accepting state to be output in the UI. Functional components can not be used to access it's own state.
 
-        // * DESTRUCTURING PROPERTIES / PROPS
-        // ? simpan props name ke variabel bernama "name", dst (nama variabel harus sama dengan nama props)
-        // ? Destructuring memungkinkan agar tidak perlu selalu memanggil props dengan "this.state"
-        // const { name, age, belt } = this.props;
+const Ninjas = ({ ninjas }) => { // ? auto destructuring props menjadi const ninjas
 
-        const { ninjas } = this.props;
-        // const ninjas = this.props.ninjas;
+    // * Dalam CLASS-BASED COMPONENTS, props secara otomatis terikat pada instance component, sehingga harus dipanggil menggunakan "this"
 
-        // * Membuat konstanta array yang menyimpan per baris data array ninjas (array 2 dimensi menjadi 1 dimensi)
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className="ninja" key={ ninja.id }>
-                    <div>Name: { ninja.name }</div>
-                    <div>Age: { ninja.age }</div>
-                    <div>Belt: { ninja.belt }</div>
-                    <hr />
-                </div>
-            )
-        })
+    // * DESTRUCTURING PROPERTIES / PROPS
+    // ? simpan props name ke variabel bernama "name", dst (nama variabel harus sama dengan nama props)
+    // ? Destructuring memungkinkan agar tidak perlu selalu memanggil props dengan "this.state"
+    // const { name, age, belt } = this.props;
 
-        // * Menampilkan nilai konstanta array ninjaList sebanyak data mapping yang dihasilkan
-        return(
-            <div className="ninja-list">
-                { ninjaList }
+    // * Dalam FUNCTION, tidak perlu merefer pada class instance, sehingga tidak perlu menggunakan "this", dan hanya akan merefer props yang dipassing ke function sebagai parameter
+    // const { ninjas } = props;
+    // const ninjas = this.props.ninjas;
+
+    // * Membuat konstanta array yang menyimpan per baris data array ninjas (array 2 dimensi menjadi 1 dimensi)
+    const ninjaList = ninjas.map(ninja => {
+        return (
+            <div className="ninja" key={ ninja.id }>
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
+                <hr />
             </div>
         )
-    }
+    })
+
+    // * Menampilkan nilai konstanta array ninjaList sebanyak data mapping yang dihasilkan
+    return(
+        <div className="ninja-list">
+            { ninjaList }
+        </div>
+    )
 }
 
 export default Ninjas;
